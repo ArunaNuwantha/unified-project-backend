1c7fc8463339fe32913221f7acbb003550697193
arunanuwantha97 <arunanuwanthablan97@gmail.com>
2021-01-12 22:26:15 +0530

    add configuration files

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("config");
+const app = express();
+
 const userRegister = require("./routes/register");
 const userLogin = require("./routes/login");
 const patients = require("./routes/patients");
 const containers = require("./routes/containers");
-const cors = require("cors");
 
-const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/register", userRegister);
@@ -14,12 +16,8 @@ app.use("/api/login", userLogin);
 app.use("/api/patients", patients);
 app.use("/api/container", containers);
 
-const cloud_mongodb =
-  "mongodb+srv://spm-user:1234@cluster0.pps0b.mongodb.net/spmdb?retryWrites=true&w=majority";
-const local_mongodb = "mongodb://localhost/project";
-
 mongoose
-  .connect(cloud_mongodb, {
+  .connect(config.get("db"), {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
